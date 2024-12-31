6c75204d74c96af28d9306e9aea942e259c6cb8d
vinaykumar142 <130731047+vinaykumar142@users.noreply.github.com>
2024-12-31 23:52:51 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -5,7 +5,7 @@ sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
 sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
-sudo tar -xvf nexus.tar.gz
+sudo tar -zxvf nexus.tar.gz
 sudo mv nexus-3* nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
